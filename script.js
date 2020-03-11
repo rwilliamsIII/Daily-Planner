ffed74298c3bc68423c528072bbeec3c3843734f
Robert Williams <robert@a3marketing.com>
2020-03-11 18:05:46 -0500

    added document ready function

--- script.js
+++ script.js
@@ -2,45 +2,37 @@ var day = moment().format("dddd") + " " + moment()
 var hourCount = 0;
 var inputArray = [];
 var input = "";
-var credentialsContainer = getElementById("credential-container");
-
 
 // variables to dynamically create content
-var timeblockRow = $("<div>", {"class": "row"});
-var hourCol = $("<div>", {"class":"col-md-1", "hour"});
-var textArea = $("<div>", {"class":"col-md-10", "textarea"});
-var savebtnArea = $("<div>", {"class":"col-md-1"});
+// var timeblockRow = $("<div>", {"class": "row"});
+// var hourCol = $("<div>", {"class":"col-md-1", "hour"});
+// var textArea = $("<div>", {"class":"col-md-10", "textarea"});
+// var savebtnArea = $("<div>", {"class":"col-md-1"});
+
+$(document).ready(function() {
+});
+function displayDate();
 
 // using for loop to create time block elements and append them to the container
 for (i = 0; i < 12; i++) {
     hourCount ++;
-    credentialsContainer.append($(timeblockRow))
-    
-    
-    
-    
-    // ($("<div></div>").addClass("row"));
-    // $(".row:eq(" + i + ")").append($("<div></div>").addClass("col-md-1"), $("<div").addClass("col-md-10"), $("<div></div>").addClass("col-md-1"));
-    // $(".col-md-10:eq(" + i + ")").append($("<div></div>").addClass("hourSlot"), $("<div>").addClass("input-group mb-3"));
-    // $(".mg-3:eq(" + i + ")").append($("<div>").addClass("input-group-prepend"), $("<input>").addClass("form-control").attr("type", "text").attr("aria-label", "Text input with checkbox"));
-    // $(".input-group-prepend:eq(" + i + ")").append($("<input>").addClass("btn btn-primary").attr("type", "submit").attr("value", "Save"));
-    // $(".hourSlot:eq(" + i + ")").append(moment().add(i - 3, "hour").format("h a"));
+    $(".container").append($("<div></div>").addClass("row"));
+    $(".row:eq(" + i + ")").append($("<div></div>").addClass("col-md-1"), $("<div").addClass("col-md-10"), $("<div></div>").addClass("col-md-1"));
+    $(".col-md-10:eq(" + i + ")").append($("<div></div>").addClass("hourSlot"), $("<div>").addClass("input-group mb-3"));
+    $(".mg-3:eq(" + i + ")").append($("<div>").addClass("input-group-prepend"), $("<input>").addClass("form-control").attr("type", "text").attr("aria-label", "Text input with checkbox"));
+    $(".input-group-prepend:eq(" + i + ")").append($("<input>").addClass("btn btn-primary").attr("type", "submit").attr("value", "Save"));
+    $(".hourSlot:eq(" + i + ")").append(moment().add(i - 3, "hour").format("h a"));
 
-    // var hour = moment().add(i - 3, "hour").format("h a");
-    // if (hourCount <= 3) {
-    //     $(".form-control:eq(" + i + ")").css("background-color", "#cc5500")
-    // }
-    // else if (hourCount == 4) {
-    //     $(".form-control:eq(" + i + ")").css("background-color", "#654321")
-    // }
-    // else if (hourCount > 4) {
-    //     $(".form-control:eq(" + i + ")").css("background-color", "teal")
-    // }
-
-
-
-
-
+    var hour = moment().add(i - 3, "hour").format("h a");
+    if (hourCount <= 3) {
+        $(".form-control:eq(" + i + ")").css("background-color", "#cc5500")
+    }
+    else if (hourCount == 4) {
+        $(".form-control:eq(" + i + ")").css("background-color", "#654321")
+    }
+    else if (hourCount > 4) {
+        $(".form-control:eq(" + i + ")").css("background-color", "teal")
+    }
 
 
     $(".btn:eq(" + i + ")").click(function(){
