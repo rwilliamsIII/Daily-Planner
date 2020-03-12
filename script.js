e74c9262204eca60a2e93084dafdaee72076cd02
Robert Williams <robert@a3marketing.com>
2020-03-12 16:27:17 -0500

    recreated for loop to generate html elements

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var showDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
-var hourCount = 0;
+var hourCount = [
+    "9am",
+    "10am",
+    "11am",
+    "12pm",
+    "1pm",
+    "2pm",
+    "3pm",
+    "4pm",
+    "5pm"
+];
 var inputArray = [];
 var input = "";
 
@@ -18,27 +28,50 @@ console.log(showDate);
 };
 
 // created variables for the creation of dynamic html content
-    var timeBlockBox = document.querySelector(".container")
-    var timeBlock = $("<div>", {"class": "row"});
-    var hourBlock = $("<div>", {"class": "col-md-1"});
-    var textBlock = $("<div>", {"class": "col-md-10"});
-    var saveBlock = $("<div>", {"class": "col-md-1"});
-    var saveButton = $("<button>", {"class": "savebtn"});
+
+    // var timeBlockBox = document.querySelector(".container")
+    // var timeBlock = $("<div>", {"class": "row"});
+    // var hourBlock = $("<div>", {"class": "col-md-1"});
+    // var textBlock = $("<div>", {"class": "col-md-10"});
+    // var saveBlock = $("<div>", {"class": "col-md-1"});
+    // var saveButton = $("<button>", {"class": "savebtn"});
+
     // assigned classes to the the created variables
+
     // $(timeBlock).addClass("time-block");
     // $(hourBlock).addClass("hour");
     // $(textBlock).addClass("textarea");
     // $(saveBlock).html(saveButton);
     // $(timeBlock).html(hourBlock, textBlock, saveBlock);
+
+
+    // added content to newly created div
+
+    // $(hourBlock).text(hour);
+    // $()
     
 
 // using for loop to create time block elements and append them to the container
 function generatePlanner(){
-
     for (i = 0; i < 12; i++) {
         hourCount ++;
 
-        $(timeBlockBox).append(timeBlock).addClass("time-block");
-        $(timeBlock).append(hourBlock).addClass("hour");
+        $(".container").append($("<div></div>").addClass("row"));
+        $(".row:eq(" + i + ")").append($("<div></div>").addClass("col-md-1"), $("<div>").addClass("col-md-10"), $("<div></div>").addClass("col-md-1"));
+        $(".col-md-10:eq(" + i + ")").append($("<div></div>").addClass("hourSlot"), $("<div>").addClass("input-group mb-3"));
+        $(".mb-3:eq(" + i + ")").append($("<div>").addClass("input-group-prepend"), $("<input>").addClass("form-control").attr("type", "text").attr("aria-label", "Text input with checkbox"));
+        $("input-group-prepend:eq(" + i + ")").append($("<input>").addClass("btn btn-primary").attr("type", "submit").attr("value", "Save"));
+        $(".hourSlot:eq(" + i + ")").append(moment().add(i - 3, "hour").format("h a"));
+
+        
     };
+    
 };
+
+
+
+
+
+    // $.each(hourCount, function(i, hour) {
+       
+        // })
